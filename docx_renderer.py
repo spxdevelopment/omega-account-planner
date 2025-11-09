8e76cf9ad28ab20d80c45b0030164de1e3ebe9f5
Sales Sparx <salessparx.development@gmail.com>
2025-11-09 13:14:31 -0500

    Update docx_renderer.py

--- docx_renderer.py
+++ docx_renderer.py
@@ -1,16 +1,16 @@
 from docxtpl import DocxTemplate
 
-def render_template(template_path, json_data, output_path):
+def render_template_to_docx(template_path, json_data, output_path):
     tpl = DocxTemplate(template_path)
 
-    def fill_blanks(obj):
+    def fallback(obj):
         if isinstance(obj, dict):
-            return {k: fill_blanks(v) for k, v in obj.items()}
+            return {k: fallback(v) for k, v in obj.items()}
         elif isinstance(obj, list):
-            return [fill_blanks(i) for i in obj]
+            return [fallback(i) for i in obj]
         elif obj in ("", None):
             return "Not Available"
         return obj
 
-    tpl.render(fill_blanks(json_data))
+    tpl.render(fallback(json_data))
     tpl.save(output_path)
